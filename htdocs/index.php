5ee1a04c3b0079a69cb5d3ea9eb507b335d76363
Britt Gresham <brittcgresham@gmail.com>
2014-07-03 15:12:04 -0500

    Bootstrapped index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>DNS Tools</title>
-<style type="text/css">
-table, td, th {border:1px solid black;}
-td {text-align:right;}
-</style>
-</head>
-<body>
 <?php
 
 function linuxUptime() {
-  $ut = strtok( exec( "cat /proc/uptime" ), "." );
-  $days = sprintf( "%2d", ($ut/(3600*24)) );
-  $hours = sprintf( "%2d", ( ($ut % (3600*24)) / 3600) );
-  $min = sprintf( "%2d", ($ut % (3600*24) % 3600)/60  );
-  $sec = sprintf( "%2d", ($ut % (3600*24) % 3600)%60  );
-  return array( $days, $hours, $min, $sec );
+    $ut = strtok( exec( "cat /proc/uptime" ), "." );
+    $days = sprintf( "%2d", ($ut/(3600*24)) );
+    $hours = sprintf( "%2d", ( ($ut % (3600*24)) / 3600) );
+    $min = sprintf( "%2d", ($ut % (3600*24) % 3600)/60  );
+    $sec = sprintf( "%2d", ($ut % (3600*24) % 3600)%60  );
+    return array( $days, $hours, $min, $sec );
 }
 
 $ut = linuxUptime();
@@ -25,73 +14,166 @@ $ut = linuxUptime();
 $ua = get_browser();
 
 ?>
-<p>You are on server <?php echo php_uname('n');?>
-</p>
-<p>Your IP address is <?php echo $_SERVER["REMOTE_ADDR"];?>
-</p>
-<p>Your host name is <?php echo gethostbyaddr($_SERVER["REMOTE_ADDR"]);?>
-</p>
-<p>Your browser is <?php echo $ua->parent;?>
-</p>
-<p>Your operating system is <?php echo $ua->platform;?>
-</p>
-<p>Server uptime is <?php echo "$ut[0] days, $ut[1] hours, $ut[2] minutes, $ut[3] seconds."; ?>
-</p>
-<table border="1">
-<tr><th>Tool</th><th>Host Name</th></tr>
-<form name="ping" action="/cgi-bin/ping" method="get">
-<tr><td><input type="submit" value="Ping" /></td>
-<td><input type="text" name="ip" /></td></tr>
-</form>
-<form name="whois" action="/cgi-bin/whois" method="get">
-<tr><td><input type="submit" value="Who Is" /></td>
-<td><input type="text" name="ip" /></td></tr>
-</form>
-<form name="tracert" action="/cgi-bin/traceroute" method="get">
-<tr><td><input type="submit" value="Trace Route" /></td>
-<td><input type="text" name="ip" /></td></tr>
-</form>
-<form name="dig" action="/cgi-bin/dig" method="get">
-<tr><td><input type="submit" value="DNS record lookup" /></td>
-<td><input type="text" name="ip" /></td>
-<td>Type: <select name="type">
-<option value="ANY">ANY</option>
-<option value="A">A</option>
-<option value="AAAA">AAAA</option>
-<option value="CNAME">CNAME</option>
-<option value="MX">MX</option>
-<option value="NS">NS</option>
-<option value="PTR">PTR</option>
-<option value="SOA">SOA</option>
-<option value="SPF">SPF</option>
-<option value="SRV">SRV</option>
-<option value="TXT">TXT</option>
-<option value="DNSSEC">DNSSEC</option>
-</select></td></tr>
-</form>
-<form name="revdns" action="/cgi-bin/reversedig" method="get">
-<tr><td><input type="submit" value="Reverse DNS Lookup" /></td>
-<td><input type="text" name="ip" /></td></tr>
-</form>
-<form name="smtp" action="/cgi-bin/smtp" method="get">
-<tr><td><input type="submit" value="TCP/SMTP Port Test" /></td>
-<td><input type="text" name="ip" /></td></tr>
-</form>
-<form name="heartbleed" action="/cgi-bin/heartbleed" method="get">
-<tr><td><input type="submit" value="Heartbleed Test" /></td>
-<td><input type="text" name="ip" /></td></tr>
-</form>
-</table>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-XXXXXXX-XX', 'example.com');
-  ga('send', 'pageview');
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>DNS Tools</title>
+        <style type="text/css">
+            body {
+                padding-top: 15px;
+            }
+        </style>
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
+        <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
 
-</script>
-<p>Questions? Issues? Want a feature? Let me know at <a href="https://github.com/thedonkdonk/tools/issues">https://github.com/thedonkdonk/tools/issues</a>.
-</body>
+            <nav class="navbar navbar-inverse" role="navigation">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Tools</a>
+                    </div>
+                </div>
+            </nav>
+
+            <div class="col-lg-5">
+                <div class="well well">
+                    <p>You are on server <?php echo php_uname('n');?></p>
+                    <p>Your IP address is <?php echo $_SERVER["REMOTE_ADDR"];?></p>
+                    <p>Your host name is <?php echo gethostbyaddr($_SERVER["REMOTE_ADDR"]);?></p>
+                    <p>Your browser is <?php echo $ua->parent; ?></p>
+                    <p>Your operating system is <?php echo $ua->platform; ?></p>
+                    <p>Server uptime is <?php echo "$ut[0] days, $ut[1] hours, $ut[2] minutes, $ut[3] seconds."; ?></p>
+                </div>
+            </div>
+            <div class="col-lg-6">
+
+                <form class="row" action="/cgi-bin/ping" method="GET" role="form">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="text" name="ip" class="form-control" placeholder="IP Address or Hostname">
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" type="submit">Ping</button>
+                            </span>
+                        </div>
+                    </div>
+                </form>
+
+                <form class="row" action="/cgi-bin/whois" method="GET" role="form">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="text" name="ip" class="form-control" placeholder="IP Address or Hostname">
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" type="submit">Who Is</button>
+                            </span>
+                        </div>
+                    </div>
+                </form>
+
+                <form class="row" action="/cgi-bin/traceroute" method="GET" role="form">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="text" name="ip" class="form-control" placeholder="IP Address or Hostname">
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" type="submit">Trace Route</button>
+                            </span>
+                        </div>
+                    </div>
+                </form>
+
+                <form class="row" id="dnsform" action="/cgi-bin/dig" method="GET" role="form">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="text" name="ip" class="form-control" placeholder="IP Address or Hostname">
+                            <input type="hidden" name="type" id="dnstype" value="ANY"/>
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">DNS Lookup <span class="caret"></span></button>
+                                <ul class="dropdown-menu dropdown-menu-right" role="menu">
+                                    <li><a href="#">ANY</a></li>
+                                    <li><a href="#">A</a></li>
+                                    <li><a href="#">AAAA</a></li>
+                                    <li><a href="#">CNAME</a></li>
+                                    <li><a href="#">MX</a></li>
+                                    <li><a href="#">NS</a></li>
+                                    <li><a href="#">PTR</a></li>
+                                    <li><a href="#">SOA</a></li>
+                                    <li><a href="#">SPF</a></li>
+                                    <li><a href="#">SRV</a></li>
+                                    <li><a href="#">TXT</a></li>
+                                    <li><a href="#">DNSSEC</a></li>
+                                </ul>
+                            </span>
+                        </div>
+                    </div>
+                    <script type="text/javascript" charset="utf-8">
+                        $('.dropdown-menu li').click(function(e){
+                            e.preventDefault();
+                            var selected = $(this).text();
+                            $('#dnstype').val(selected);
+                            $("#dnsform").submit();
+                        });
+                    </script>
+                </form>
+
+                <form class="row" action="/cgi-bin/reversedig" method="GET" role="form">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="text" name="ip" class="form-control" placeholder="IP Address or Hostname">
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" type="submit">Reverse DNS</button>
+                            </span>
+                        </div>
+                    </div>
+                </form>
+
+                <form class="row" action="/cgi-bin/smtp" method="GET" role="form">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="text" name="ip" class="form-control" placeholder="IP Address or Hostname">
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" type="submit">TCP/SMTP Port Test</button>
+                            </span>
+                        </div>
+                    </div>
+                </form>
+
+                <form class="row" action="/cgi-bin/heartbleed" method="GET" role="form">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input type="text" name="ip" class="form-control" placeholder="IP Address or Hostname">
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" type="submit">Heartbleed Test</button>
+                            </span>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+
+            <script>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+                ga('create', 'UA-XXXXXXX-XX', 'example.com');
+                ga('send', 'pageview');
+            </script>
+        </div>
+        <div class="container">
+            <hr>
+            <p>Questions? Issues? Want a feature? Let me know at <a href="https://github.com/thedonkdonk/tools/issues">https://github.com/thedonkdonk/tools/issues</a>.</p>
+        </div>
+    </body>
 </html>
